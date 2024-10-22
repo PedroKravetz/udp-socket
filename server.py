8b7314e88a23efb633ea71c22d6dd87bf82e8251
PedroKravetz <p.v.kravetz@gmail.com>
2024-10-22 20:04:20 -0300

    Exception

--- server.py
+++ server.py
@@ -26,46 +26,49 @@ server_socket.bind((IP, PORT))
 print(f"Servidor UDP rodando em {IP}:{PORT}")
 
 while True:
-    # Recebe requisição do cliente
-    data, address = server_socket.recvfrom(BUFFER_SIZE)
-    request = data.decode('utf-8')
+    try:
+        # Recebe requisição do cliente
+        data, address = server_socket.recvfrom(BUFFER_SIZE)
+        request = data.decode('utf-8')
 
-    # Se a requisição for válida
-    if request.startswith("REQU"):
-        _, file_name = request.split(" ")
-        
-        if os.path.exists(DIR+file_name):
-            # Divide o arquivo em pedaços
-            total_size = os.path.getsize(DIR+file_name)
-            total_blocks = (total_size // BUFFER_SIZE) + 1
-            block_number = 1
+        # Se a requisição for válida
+        if request.startswith("REQU"):
+            _, file_name = request.split(" ")
             
-            for block in divide_file(DIR+file_name, BUFFER_SIZE):
-                checksum = generate_checksum(block)
-                response = f"DATA {block_number}/{total_blocks} {checksum} ".encode('utf-8') + block
-                server_socket.sendto(response, address)
-                block_number += 1
-        else:
-            # Se o arquivo não for encontrado
-            error_msg = "ERRO Arquivo não encontrado".encode('utf-8')
-            server_socket.sendto(error_msg, address)
-    elif request.startswith("SEND"):
-        _, block_required, file_name = request.split(" ")
-        print(block_required)
-        print(file_name)
-        if os.path.exists(DIR+file_name):
-            # Divide o arquivo em pedaços
-            total_size = os.path.getsize(DIR+file_name)
-            total_blocks = (total_size // BUFFER_SIZE) + 1
-            block_number = 1
-            
-            for block in divide_file(DIR+file_name, BUFFER_SIZE):
-                checksum = generate_checksum(block)
-                if int(block_required)==block_number:
+            if os.path.exists(DIR+file_name):
+                # Divide o arquivo em pedaços
+                total_size = os.path.getsize(DIR+file_name)
+                total_blocks = (total_size // BUFFER_SIZE) + 1
+                block_number = 1
+                
+                for block in divide_file(DIR+file_name, BUFFER_SIZE):
+                    checksum = generate_checksum(block)
                     response = f"DATA {block_number}/{total_blocks} {checksum} ".encode('utf-8') + block
                     server_socket.sendto(response, address)
-                block_number += 1
-        else:
-            # Se o arquivo não for encontrado
-            error_msg = "ERRO Arquivo não encontrado".encode('utf-8')
-            server_socket.sendto(error_msg, address)
+                    block_number += 1
+            else:
+                # Se o arquivo não for encontrado
+                error_msg = "ERRO Arquivo não encontrado".encode('utf-8')
+                server_socket.sendto(error_msg, address)
+        elif request.startswith("SEND"):
+            _, block_required, file_name = request.split(" ")
+            print(block_required)
+            print(file_name)
+            if os.path.exists(DIR+file_name):
+                # Divide o arquivo em pedaços
+                total_size = os.path.getsize(DIR+file_name)
+                total_blocks = (total_size // BUFFER_SIZE) + 1
+                block_number = 1
+                
+                for block in divide_file(DIR+file_name, BUFFER_SIZE):
+                    checksum = generate_checksum(block)
+                    if int(block_required)==block_number:
+                        response = f"DATA {block_number}/{total_blocks} {checksum} ".encode('utf-8') + block
+                        server_socket.sendto(response, address)
+                    block_number += 1
+            else:
+                # Se o arquivo não for encontrado
+                error_msg = "ERRO Arquivo não encontrado".encode('utf-8')
+                server_socket.sendto(error_msg, address)
+    except ConnectionResetError:
+        continue
